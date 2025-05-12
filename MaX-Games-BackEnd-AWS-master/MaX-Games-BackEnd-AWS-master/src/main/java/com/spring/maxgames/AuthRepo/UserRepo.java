eab314492d581eed83c872f3808836cd383d50d3
Vamsi krishnan <vamsikrishnan7111@gmail.com>
2025-05-12 14:20:36 +0530

    Delete MaX-Games-BackEnd-AWS-master/MaX-Games-BackEnd-AWS-master directory

--- MaX-Games-BackEnd-AWS-master/MaX-Games-BackEnd-AWS-master/src/main/java/com/spring/maxgames/AuthRepo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.spring.maxgames.AuthRepo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring.maxgames.AuthModel.User;
-
-public interface UserRepo extends JpaRepository<User, Integer> {
-	User findByUsername(String username);
-}
--- MaX-Games-BackEnd-AWS-master/MaX-Games-BackEnd-AWS-master/src/main/java/com/spring/maxgames/EventRepo/EventsRepo.java
+++
@@ -1,9 +0,0 @@
-package com.spring.maxgames.EventRepo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.spring.maxgames.EventModel.Events;
-
-public interface EventsRepo extends JpaRepository<Events, Long> {
-
-}
